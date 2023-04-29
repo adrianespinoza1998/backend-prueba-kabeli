a1d7625a5656261342f80b9429a661f3d95d87fa
Adrian Espinoza <adrianespinozaarevalo@gmail.com>
2023-04-29 11:18:10 -0400

    add indicador controller test

--- src/indicadores/indicadores.controller.spec.ts
+++ src/indicadores/indicadores.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { IndicadoresController } from './indicadores.controller';
+import { IndicadoresService } from './indicadores.service';
+import { testingValues } from './testingValues';
 
 describe('IndicadoresController', () => {
   let controller: IndicadoresController;
@@ -7,6 +9,7 @@ describe('IndicadoresController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [IndicadoresController],
+      providers: [IndicadoresService],
     }).compile();
 
     controller = module.get<IndicadoresController>(IndicadoresController);
@@ -14,5 +17,15 @@ describe('IndicadoresController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should return the indicators', async () => {
+    const expectedResults = testingValues;
+
+    jest.spyOn(controller, 'getIndicadores').mockResolvedValue(expectedResults);
+
+    const results = await controller.getIndicadores();
+
+    expect(results).toEqual(expectedResults);
   });
 });
